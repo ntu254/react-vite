d0791a88f1cfb640a23670284fd7663b8abe8224
ntu254 <nttu254.vn@gmail.com>
2025-06-16 17:58:24 +0700

    #5. JSX, fragment

--- src/components/learn/MyComponent.jsx
+++ src/components/learn/MyComponent.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import './style.css'; //import css file
 
 //() => {} 
 // () : tham so dau vao
@@ -7,9 +8,20 @@ import React from 'react';
 
 //component = html + css + js
 
+//Fragment: <></> : khong can phai dung <div> bao quanh
+//JSX: JavaScript XML, cho phep viet HTML trong JavaScript
+//JSX: chi ho phep viet mot phan tu goc, neu muon viet nhieu phan tu thi phai dung Fragment
+
 const MyComponent = () => {
   return (
-    <div>Nguyen Tuan Tu update</div> //tag
+    <>
+      <div>Nguyen Tuan Tu update</div>
+      <div className='child'
+        style = {
+          { borderRadius: "10px" }
+        }
+      >Mau do</div>
+    </>
   );
 }
 
